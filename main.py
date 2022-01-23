891590ab7b8ef9257a2891b0d1d2d9887c03ee8f
abarzenok <69277070+abarzenok@users.noreply.github.com>
2022-01-23 20:17:27 +0300

    Add images folder creation with its existence check

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import os
+from pathlib import Path
 import requests
+
 
 
 def main():
     image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
     image_name = "hubble.jpg"
+    image_dir = "images"
+    full_path = os.path.join(image_dir, image_name)
+
+    Path(image_dir).mkdir(exist_ok=True)
 
     response = requests.get(image_url)
     response.raise_for_status()
 
-    with open(image_name, "wb") as file:
+    with open(full_path, "wb") as file:
         file.write(response.content)
 
 

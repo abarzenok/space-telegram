d16d9d26c1ae9fa68ff393d2ec3f0527c9077dbe
abarzenok <69277070+abarzenok@users.noreply.github.com>
2022-01-23 20:38:43 +0300

    Add download_image function

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from pathlib import Path
 import requests
 
 
-
-def main():
-    image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    image_name = "hubble.jpg"
-    image_dir = "images"
+def download_image(image_url, image_dir, image_name):
     full_path = os.path.join(image_dir, image_name)
-
     Path(image_dir).mkdir(exist_ok=True)
 
     response = requests.get(image_url)
@@ -17,6 +12,18 @@ def main():
 
     with open(full_path, "wb") as file:
         file.write(response.content)
+
+
+def main():
+    image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    image_dir = "images"
+    image_name = "hubble.jpg"
+
+    download_image(
+        image_url=image_url,
+        image_dir=image_dir,
+        image_name=image_name,
+    )
 
 
 if __name__ == '__main__':

03f5c4278fadeb6553f47bf2dfee3be06b4150b8
abarzenok <69277070+abarzenok@users.noreply.github.com>
2022-01-24 10:59:12 +0300

    Add SpaceX images download functions

--- main.py
+++ main.py
@@ -14,16 +14,35 @@ def download_image(image_url, image_dir, image_name):
         file.write(response.content)
 
 
-def main():
-    image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    image_dir = "images"
-    image_name = "hubble.jpg"
+def get_images_urls():
+    launches_url = "https://api.spacexdata.com/v4/launches"  # TODO: parametrize as function argument
+    response = requests.get(launches_url)
+    response.raise_for_status()
+    launches = response.json()
+    images_urls = []
 
-    download_image(
-        image_url=image_url,
-        image_dir=image_dir,
-        image_name=image_name,
-    )
+    for launch in launches:
+        if launch.get("links").get("flickr").get("original"):
+            images_urls = launch["links"]["flickr"]["original"]  # TODO: try to access dict only once
+            break
+
+    return images_urls
+
+
+def fetch_spacex_last_launch():
+    image_dir = "images"
+    image_name = "spacex{}.jpg"
+    images_urls = get_images_urls()
+    for index, image_url in enumerate(images_urls, start=1):
+        download_image(
+            image_url=image_url,
+            image_dir=image_dir,
+            image_name=image_name.format(index),
+        )
+
+
+def main():
+    fetch_spacex_last_launch()
 
 
 if __name__ == '__main__':
